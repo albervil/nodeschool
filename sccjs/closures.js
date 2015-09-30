1e4ded4cd9d2e7552169d2827763f37be1242b0a
Alberto Vila <alberto.vilatena@gmail.com>
2015-09-30 16:38:49 +0300

    Scope chains and closures exercises

---
+++ sccjs/closures.js
@@ -0,0 +1,9 @@
+function foo(){
+	var bar;
+	quux = 33;
+	function zip(){
+		var quux = 25;
+		bar = true;
+	}
+	return zip;
+}
---
+++ sccjs/global-scopes.js
@@ -0,0 +1,7 @@
+function foo(){
+	var bar;
+	quux = 33;
+	function zip(){
+		var quux = 25;
+	}
+}
